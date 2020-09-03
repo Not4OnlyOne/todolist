f4247c4a640832531fd2cdd057236226a5ee50d2
amoldalwai <amoldalwaiyt@gmail.com>
2020-09-04 01:51:24 +0530

    Updates

---
+++ precache-manifest.a353f197d752711f17ffd7c9f6d17216.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e5b8ecb774bee27c89ad",
+    "url": "/todolist/static/css/main.f8c5c692.chunk.css"
+  },
+  {
+    "revision": "e5b8ecb774bee27c89ad",
+    "url": "/todolist/static/js/main.72ebcd8f.chunk.js"
+  },
+  {
+    "revision": "5225356577f332e6df46",
+    "url": "/todolist/static/js/runtime~main.d3bf418b.js"
+  },
+  {
+    "revision": "e9ad34627dee6dd58b4e",
+    "url": "/todolist/static/js/2.e99b8eec.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/todolist/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "163602799e09df44e91a8f97fe160942",
+    "url": "/todolist/index.html"
+  }
+];
